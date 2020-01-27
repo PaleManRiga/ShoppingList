bc4522c177ee3896d8019636f4200c6671e966cc
Sergejs <sergejs_rudenko@inbox.lv>
2020-01-27 13:08:37 +0200

    Initial Project

--- src/com/javaguru/shoppinglist/Product.java
+++ src/com/javaguru/shoppinglist/Product.java
@@ -1,12 +1,40 @@
 package com.javaguru.shoppinglist;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
 
     private Long id;
     private String name;
     private BigDecimal price;
+    private String category;
+    private BigDecimal discount;
+    private String description;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount.divide(new BigDecimal(100));
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public Long getId() {
         return id;
@@ -32,4 +60,33 @@ public class Product {
         this.price = price;
     }
 
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", discount=" + discount +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) &&
+                Objects.equals(name, product.name) &&
+                Objects.equals(price, product.price) &&
+                Objects.equals(category, product.category) &&
+                Objects.equals(discount, product.discount) &&
+                Objects.equals(description, product.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, category, discount, description);
+    }
 }
--- src/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -10,32 +10,72 @@ class ShoppingListApplication {
     public static void main(String[] args) {
         Map<Long, Product> productRepository = new HashMap<>();
         Long productIdSequence = 0L;
+        ProductValidationService service = new ProductValidationService();
         while (true) {
             Scanner scanner = new Scanner(System.in);
             try {
                 System.out.println("1. Create product");
                 System.out.println("2. Find product by id");
-                System.out.println("3. Exit");
+                System.out.println("3. Delete product by id");
+                System.out.println("4. Edit product");
+                System.out.println("5. Exit");
                 Integer userInput = Integer.valueOf(scanner.nextLine());
                 switch (userInput) {
                     case 1:
+                        Product product = new Product();
                         System.out.println("Enter product name: ");
-                        String name = scanner.nextLine();
+                        product.setName(scanner.nextLine());
                         System.out.println("Enter product price: ");
                         BigDecimal price = new BigDecimal(scanner.nextLine());
-                        Product product = new Product();
-                        product.setName(name);
                         product.setPrice(price);
+                        System.out.println("Enter product discount: ");
+                        BigDecimal discount = new BigDecimal(scanner.nextLine());
+                        product.setDiscount(discount);
+                        System.out.println("Enter  product category: ");
+                        product.setCategory(scanner.nextLine());
+                        System.out.println("Enter product description");
+                        product.setDescription(scanner.nextLine());
+                        service.validate(product);
                         product.setId(productIdSequence);
                         productRepository.put(productIdSequence, product);
                         productIdSequence++;
-                        System.out.println("Result: " + product.getId());
+                        System.out.println("Product ID: " + product.getId());
+                        break;
                     case 2:
                         System.out.println("Enter product id: ");
                         long id = scanner.nextLong();
                         Product findProductResult = productRepository.get(id);
                         System.out.println(findProductResult);
+                        break;
                     case 3:
+                        System.out.println("Enter product id: ");
+                        id = scanner.nextLong();
+                        productRepository.remove(id);
+                        break;
+                    case 4:
+                        Product newProduct = new Product();
+                        System.out.println("Enter product id");
+                        id = scanner.nextLong();
+                        scanner.nextLine();
+                        findProductResult = productRepository.get(id);
+                        System.out.println(findProductResult);
+                        System.out.println("Enter product name: ");
+                        newProduct.setName(scanner.nextLine());
+                        System.out.println("Enter product price: ");
+                        price = new BigDecimal(scanner.nextLine());
+                        newProduct.setPrice(price);
+                        System.out.println("Enter product discount: ");
+                        discount = new BigDecimal(scanner.nextLine());
+                        newProduct.setDiscount(discount);
+                        System.out.println("Enter  product category: ");
+                        newProduct.setCategory(scanner.nextLine());
+                        System.out.println("Enter product description");
+                        newProduct.setDescription(scanner.nextLine());
+                        service.validate(newProduct);
+                        newProduct.setId(id);
+                        productRepository.replace(id,newProduct);
+                        break;
+                        case 5:
                         return;
                 }
             } catch (Exception e) {
